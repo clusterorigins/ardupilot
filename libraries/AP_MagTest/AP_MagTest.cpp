567b28ac55e7b140edc2bf421822c665f0f2a680
clusterorigins <86234791+clusterorigins@users.noreply.github.com>
2021-10-10 13:10:01 +0200

    Update AP_MagTest.cpp

--- libraries/AP_MagTest/AP_MagTest.cpp
+++ libraries/AP_MagTest/AP_MagTest.cpp
@@ -1,1 +1,28 @@
+#include <AP_BoardConfig/AP_BoardConfig.h>
+#include <AP_Logger/AP_Logger.h>
+#include <AP_Vehicle/AP_Vehicle_Type.h>
 
+extern const AP_HAL::HAL &hal;
+
+???const AP_Param::GroupInfo RangeFinder::var_info[] = {???
+    
+    
+MagTest::MagTest()
+{
+    AP_Param::setup_object_defaults(this, var_info);
+}
+
+void MagTest::update(void)
+{
+    for (uint8_t i=0; i<num_instances; i++) {
+        if (drivers[i] != nullptr) {
+            if ((Type)params[i].type.get() == Type::NONE) {
+                // allow user to disable a rangefinder at runtime
+                state[i].status = Status::NotConnected;
+                state[i].range_valid_count = 0;
+                continue;
+            }
+            drivers[i]->update();
+        }
+    }
+}
