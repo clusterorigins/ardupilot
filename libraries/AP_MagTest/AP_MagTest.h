3415ab11621b80cb6961b75a1d56a77517ea0b09
clusterorigins <86234791+clusterorigins@users.noreply.github.com>
2021-12-16 13:35:04 +0100

    Update AP_MagTest.h

--- libraries/AP_MagTest/AP_MagTest.h
+++ libraries/AP_MagTest/AP_MagTest.h
@@ -1,1 +1,8 @@
+#pragma once
 
+#include <AP_Common/AP_Common.h>
+#include <AP_HAL/AP_HAL.h>
+#include <AP_Param/AP_Param.h>
+#include <GCS_MAVLink/GCS.h>
+#include <AP_MSP/msp.h>
+#include "AP_RangeFinder_Params.h"
